9ebca6615d7f01e050cfe69f01a139e0907a7963
Marcos Vasquez <marcos.vasquez.diaz@est.una.ac.cr>
2023-09-18 23:54:59 -0600

    acercade listo

---
+++ Instrumentos/src/main/java/instrumentos/presentation/AcercaDe/Controller.java
@@ -0,0 +1,20 @@
+package instrumentos.presentation.AcercaDe;
+
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+import instrumentos.Application;
+import instrumentos.logic.Service;
+import instrumentos.logic.TipoInstrumento;
+
+import java.io.FileOutputStream;
+import java.util.List;
+
+public class Controller{
+
+    public Controller(View view, Model model) {
+
+    }
+
+}
---
+++ Instrumentos/src/main/java/instrumentos/presentation/AcercaDe/Model.java
@@ -0,0 +1,11 @@
+package instrumentos.presentation.AcercaDe;
+
+import instrumentos.logic.TipoInstrumento;
+
+import java.util.List;
+import java.util.Observer;
+
+public class Model extends java.util.Observable {
+    public Model() {
+    }
+}
